fccb909ca46622e39ec3b291e949aec9fa595b5b
Tyler Paneitz <Tyler.Paneitz@vu.com>
2022-04-05 13:43:25 -0500

    fixed name issues.

--- counter.py
+++ counter.py
@@ -1,9 +1,19 @@
 def countEmail():
     # This first line is provided for you
     name = input("Enter file:")
-    if len(name) < 1 : name = "mbox-short.txt"
-    handle = open(name)
-        
+    file_open = open(name)
+    fr = file_open.readlines()
+    froms ={}
+    for n in fr:
+        if n.startswith("From "):
+            n = n.strip()
+            n = n.split(' ')
+            if n[1] in froms:
+                froms[n[1]] += 1
+            else:
+                froms[n[1]] = 1
+    maxSender = max(froms, key=froms.get)
+    print(maxSender + " " + str(froms[maxSender]))
 ## if you want to test locally before you try to sync
 ## uncomment countEmail() and run > python counter.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
